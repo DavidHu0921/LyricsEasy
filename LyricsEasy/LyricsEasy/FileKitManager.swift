c38d5ed19827d5892484673716d1c78798a79d69
DavidHu0921 <davidhu920921@gmail.com>
2017-02-09 11:31:56 +0800

    update launch screen

---
+++ LyricsEasy/LyricsEasy/FileKitManager.swift
@@ -0,0 +1,18 @@
+//
+//  FileKitManager.swift
+//  LyricsEasy
+//
+//  Created by David Hu on 2017/1/24.
+//  Copyright © 2017年 David Hu. All rights reserved.
+//
+
+import Foundation
+import FileKit
+
+public struct LyricsFile {
+    
+}
+
+class FileKitManager: NSObject {
+    let lpath = Path("~")
+}
