78ce4a4e8e045f2f8128a7ba28bfe41cd47254fb
DavidHu <DavidHu@David-Hu-deMacBook-Pro.local>
2017-02-11 23:41:23 +0800

    add strut

--- LyricsEasy/LyricsEasy/FileKitManager.swift
+++ LyricsEasy/LyricsEasy/FileKitManager.swift
@@ -10,9 +10,17 @@ import Foundation
 import FileKit
 
 public struct LyricsFile {
+    var basicPath:String = ""
+    var lyricsTitle:String = ""
+    var lyricsContent:String = ""
+    var lyricsContentForShow:String = ""
     
 }
 
 class FileKitManager: NSObject {
     let lpath = Path("~")
+    
+    let textFiles = Path.userDesktop.find(searchDepth: 5) { path in
+        path.pathExtension == "txt"
+    }
 }
